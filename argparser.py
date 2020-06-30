616ba21ad3a7ac99347b342c2247aaed3d863f6f
zaliven <64747856+zaliven@users.noreply.github.com>
2020-06-30 16:51:50 +0300

    Updated support for bruteforce

--- argparser.py
+++ argparser.py
@@ -2,25 +2,24 @@ import argparse
 import os.path
 import sys
 
-class Parser:
-    def __init__(self):
-        parser = argparse.ArgumentParser(description="PWDump Cracker")
-        parser.add_argument("-f", dest="filename", required=True, help="Enter the pwdump file you want to use", metavar="FILE", type=lambda x: self.is_valid_file(parser, x))
-        parser.add_argument("-method", dest="method", required=True, help="Enter desired method (bruteforce/wordlist)", metavar="METHOD", choices=["bruteforce", "wordlist"])
+def is_valid_file(parser, arg):
+    if not os.path.exists(arg):
+        parser.error("The file %s does not exist" % arg)
+    else:
+        return open(arg, 'r')
 
-        if (0 <= 4 <= len(sys.argv) and sys.argv[4] == "wordlist"):
-            parser.add_argument("-wordlist", dest="wordlist", required=True, help="Enter the wordlist file you want to use", metavar="WORDLIST", type=lambda x: self.is_valid_file(parser, x))
-
-        self.args = parser.parse_args()
-
-    def is_valid_file(self, parser, arg):
-        if not os.path.exists(arg):
-            parser.error("The file %s does not exist" % arg)
+def get_cli_args():
+    parser = argparse.ArgumentParser(description="PWDump Cracker")
+    parser.add_argument("-f", dest="filename", required=True, help="Enter pwdump file to crack", type=lambda x: is_valid_file(parser, x))
+    parser.add_argument("-method", dest="method", required=True, help="Enter desired method", choices=["bruteforce", "wordlist"])
+    if(0 <= 4 <= len(sys.argv)):
+        if (sys.argv[4] == "wordlist"):
+            parser.add_argument("-wordlist", dest="wordlist", required=True, help="Enter the wordlist file you want to use", type=lambda x: is_valid_file(parser, x))
         else:
-            return open(arg, 'r')
+            parser.add_argument("-range", dest="range", required=True, help="Enter the bruteforce pass length")
+            parser.add_argument("-mode", dest="mode", required=True, help="Enter bruteforce mode (you may concatenate modes): 1 - numeric | 2 - lower alpha | 3 - upper alpha | 4 - symbols", metavar="MODE")
+            parser.add_argument('-v', '--verbose', action="store_true", help="View bruteforce output")            
+    return parser.parse_args()
 
 
-    
-#args.filename.name - file name
-#args.method - method
-#args.wordlist.name - wordlist var
+
